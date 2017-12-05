c8f2f5b050e6fc10a8653550c4e2616edcfb36e2
JimmyLin39 <jimmylin0309@email.com>
2017-12-04 18:58:03 -0800

    puts player's life after check the answer

--- game.rb
+++ game.rb
@@ -18,10 +18,20 @@ class Game
   end
 
   def start
-    @players.each do |player|
-
-      puts "Hello #{player.name}"
+    player1 = @players[0]
+    player2 = @players[1]
+    puts "#{current_player.name}: Whart does 5 plus 3 equal?"
+    if gets.chomp != '8'
+      lose_life
+      puts "#{current_player.name}: Seriously? No!"
+      puts "#{player1.name}: #{player1.life}/3 vs #{player2.name}: #{player2.life}/3"
+    else
+      puts "#{current_player.name}: YES! You are correct."
     end
+  end
+
+  def lose_life
+    current_player.lose_life
   end
 
 
