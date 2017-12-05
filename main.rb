76f5da4a7d3ff34a43b72f425749dc9cd0a132f6
JimmyLin39 <jimmylin0309@email.com>
2017-12-04 17:58:51 -0800

    require game and start a new game

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
 
-requires './player'
+require './player'
+require './game'
 
 # Test code
+game = Game.new
+game.start
