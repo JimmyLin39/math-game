337bdca669460852971283188480d5ae1446099c
JimmyLin39 <jimmylin0309@email.com>
2017-12-04 19:02:22 -0800

    print life and new_turn

--- game.rb
+++ game.rb
@@ -18,20 +18,32 @@ class Game
   end
 
   def start
-    player1 = @players[0]
-    player2 = @players[1]
     puts "#{current_player.name}: Whart does 5 plus 3 equal?"
     if gets.chomp != '8'
       lose_life
       puts "#{current_player.name}: Seriously? No!"
-      puts "#{player1.name}: #{player1.life}/3 vs #{player2.name}: #{player2.life}/3"
+      print_life
+      print_new_turn
     else
       puts "#{current_player.name}: YES! You are correct."
+      print_life
+      print_new_turn
     end
+
   end
 
   def lose_life
     current_player.lose_life
+  end
+
+  def print_life
+    player1 = @players[0]
+    player2 = @players[1]
+    puts "#{player1.name}: #{player1.life}/3 vs #{player2.name}: #{player2.life}/3"
+  end
+
+  def print_new_turn
+    puts '----- NEW TURN -----'
   end
 
 
